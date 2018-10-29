48769a00c0547eab3e84e3f63264a4909f7c7048
Pham thanh Tu <tubackkhoa@gmail.com>
2018-10-29 18:26:25 +0700

    update server

--- cmd/serve.go
+++ cmd/serve.go
@@ -53,6 +53,19 @@ func run(cmd *cobra.Command, args []string) {
 
 	// gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+	// r.Use(func(c *gin.Context) {
+	// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	// 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+	// 	if c.Request.Method == "OPTIONS" {
+	// 		c.AbortWithStatus(204)
+	// 		return
+	// 	}
+
+	// 	c.Next()
+	// })
 	err = RegisterRouter(r, provider, redisConn, rabbitConn)
 	if err != nil {
 		panic(err)
