46581e18c05d2b771752bde0f658eb1656d8e6fa
Hoang Trinh <hoang.trinhj@gmail.com>
2019-03-08 11:07:55 +0700

    Backup Code

--- deployment/seed_config.js
+++ deployment/seed_config.js
@@ -1,35 +1,38 @@
-const faker = require('faker')
-const MongoClient = require('mongodb').MongoClient
-const Long = require('mongodb').Long
-const { DB_NAME, mongoUrl } = require('./utils/config')
+// const faker = require('faker')
+// const MongoClient = require('mongodb').MongoClient
+// const Long = require('mongodb').Long
+// const { DB_NAME, mongoUrl } = require('./utils/config')
+const { getEthereumBlockNumber } = require('./utils/helpers')
 
 const create = async () => {
-  const client = await MongoClient.connect(
-    mongoUrl,
-    { useNewUrlParser: true },
-  )
-
-  const db = client.db(DB_NAME)
-  await db.createCollection('config')
-
-  const index = await db
-    .collection('config')
-    .createIndex({ key: 1 }, { unique: true })
-
-  const createdAt = new Date(faker.fake('{{date.recent}}'))
-  const documents = [
-    { key: 'schema_version', value: Long.fromInt(2), createdAt },
-    { key: 'ethereum_last_block', value: Long.fromInt(0), createdAt },
-    { key: 'ethereum_address_index', value: Long.fromInt(0), createdAt },
-  ]
-  console.log(documents)
-  try {
-    await db.collection('config').insertMany(documents)
-  } catch (e) {
-    console.log(e)
-  } finally {
-    client.close()
-  }
+  const ethereumBlockNumber = await getEthereumBlockNumber(networkName)
+  console.log(ethereumBlockNumber)
+  // const client = await MongoClient.connect(
+  //   mongoUrl,
+  //   { useNewUrlParser: true },
+  // )
+  //
+  // const db = client.db(DB_NAME)
+  // await db.createCollection('config')
+  //
+  // const index = await db
+  //   .collection('config')
+  //   .createIndex({ key: 1 }, { unique: true })
+  //
+  // const createdAt = new Date(faker.fake('{{date.recent}}'))
+  // const documents = [
+  //   { key: 'schema_version', value: Long.fromInt(2), createdAt },
+  //   { key: 'ethereum_last_block', value: Long.fromInt(ethereumBlockNumber), createdAt },
+  //   { key: 'ethereum_address_index', value: Long.fromInt(0), createdAt },
+  // ]
+  // console.log(documents)
+  // try {
+  //   await db.collection('config').insertMany(documents)
+  // } catch (e) {
+  //   console.log(e)
+  // } finally {
+  //   client.close()
+  // }
 }
 
 create()
