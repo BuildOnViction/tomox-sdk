e327f2d1b89fa1e1a51822e5e5125e0b802a2a23
Hoang Trinh <hoang.trinhj@gmail.com>
2019-04-24 10:59:42 +0700

    Done Verify Signature

--- middlewares/verifysig.go
+++ middlewares/verifysig.go
@@ -1,43 +1,30 @@
 package middlewares
 
 import (
-	"bytes"
-	"fmt"
 	"net/http"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/tomochain/tomodex/utils"
+	"github.com/tomochain/tomodex/utils/httputils"
 )
 
 func VerifySignature(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// TODO: Logic to verify the signature of user here
-		if r.Header["Signature"] != nil && r.Header["Hash"] != nil && r.Header["Pubkey"] != nil {
+		if r.Header["Signature"] == nil || r.Header["Hash"] == nil || r.Header["Pubkey"] == nil {
+			httputils.WriteError(w, http.StatusUnauthorized, "There is not enough parameters in header")
+			return
+		}
 
-			signature := common.Hex2Bytes(r.Header["Signature"][0])
-			hash := common.Hex2Bytes(r.Header["Hash"][0])
-			publicKeyBytes := common.Hex2Bytes(r.Header["Pubkey"][0])
+		hash := common.Hex2Bytes(r.Header["Hash"][0])
+		signature := common.Hex2Bytes(r.Header["Signature"][0])
+		publicKeyBytes := common.Hex2Bytes(r.Header["Pubkey"][0])
 
-			utils.Logger.Debug(signature)
-			utils.Logger.Debug(hash)
-			utils.Logger.Debug(publicKeyBytes)
+		signatureNoRecoverID := signature[:len(signature)-1] // remove recovery id
+		verified := crypto.VerifySignature(publicKeyBytes, hash, signatureNoRecoverID)
 
-			sigPublicKey, err := crypto.Ecrecover(hash, signature)
-			if err != nil {
-				utils.Logger.Error(err)
-			}
-
-			matches := bytes.Equal(sigPublicKey, publicKeyBytes)
-			fmt.Println(matches) // true
-
-			if !matches {
-				w.WriteHeader(http.StatusUnauthorized)
-				return
-			}
-		} else {
-			w.WriteHeader(http.StatusUnauthorized)
+		if !verified {
+			httputils.WriteError(w, http.StatusUnauthorized, "Signature Invalid")
 			return
 		}
 
