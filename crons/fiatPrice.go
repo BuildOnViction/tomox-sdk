f206348c31c19c25169849bbd8443618340b61af
Hoang Trinh <hoang.trinhj@gmail.com>
2019-06-04 14:24:44 +0700

    Fix Cron Pattern

--- crons/fiatPrice.go
+++ crons/fiatPrice.go
@@ -7,7 +7,7 @@ import (
 // tickStreamingCron takes instance of cron.Cron and adds tickStreaming
 // crons according to the durations mentioned in config/app.yaml file
 func (s *CronService) getFiatPriceCron(c *cron.Cron) {
-	c.AddFunc("* */30 * * * *", s.updateFiatPrice())
+	c.AddFunc("0 */30 * * * *", s.updateFiatPrice())
 	c.AddFunc("0 0 * * * *", s.syncFiatPrice())
 }
 
