1ceef2a92f7a7c98e9473e68e82c8d6fce84af0f
tran nghia <nghiatran@tomochain.com>
2019-08-22 15:04:05 +0700

    paging, filter trade

--- endpoints/trade.go
+++ endpoints/trade.go
@@ -35,61 +35,79 @@ func (e *tradeEndpoint) HandleGetTrades(w http.ResponseWriter, r *http.Request) 
 	v := r.URL.Query()
 	bt := v.Get("baseToken")
 	qt := v.Get("quoteToken")
-	l := v.Get("limit")
 	fromParam := v.Get("from")
 	toParam := v.Get("to")
 
-	if bt == "" {
-		httputils.WriteError(w, http.StatusBadRequest, "baseToken Parameter missing")
-		return
+	pageOffset := v.Get("pageOffset")
+	pageSize := v.Get("pageSize")
+	sortBy := v.Get("sortBy")
+	sortType := v.Get("sortType")
+
+	sortedList := make(map[string]string)
+	sortedList["time"] = "createdAt"
+
+	var tradeSpec types.TradeSpec
+	if bt != "" {
+		if !common.IsHexAddress(bt) {
+			httputils.WriteError(w, http.StatusBadRequest, "Invalid base token address")
+			return
+		} else {
+			tradeSpec.BaseToken = bt
+		}
 	}
 
-	if qt == "" {
-		httputils.WriteError(w, http.StatusBadRequest, "quoteToken Parameter missing")
-		return
+	if qt != "" {
+		if !common.IsHexAddress(qt) {
+			httputils.WriteError(w, http.StatusBadRequest, "Invalid base token address")
+			return
+		} else {
+			tradeSpec.QuoteToken = qt
+		}
 	}
 
-	if !common.IsHexAddress(bt) {
-		httputils.WriteError(w, http.StatusBadRequest, "Invalid base token address")
-		return
-	}
-
-	if !common.IsHexAddress(qt) {
-		httputils.WriteError(w, http.StatusBadRequest, "Invalid quote token address")
-		return
-	}
-
-	// Client must provides both "from" and "to" or none of them
-	if (fromParam != "" && toParam == "") || (toParam != "" && fromParam == "") {
-		httputils.WriteError(w, http.StatusBadRequest, "Both \"from\" and \"to\" are required")
-		return
-	}
-
-	var from, to int64
-	now := time.Now()
-
-	if toParam == "" {
-		to = now.Unix()
-	} else {
+	if toParam != "" {
 		t, _ := strconv.Atoi(toParam)
-		to = int64(t)
+		tradeSpec.DateTo = int64(t)
+	}
+	if fromParam != "" {
+		t, _ := strconv.Atoi(fromParam)
+		tradeSpec.DateFrom = int64(t)
 	}
 
-	if fromParam == "" {
-		from = now.AddDate(-1, 0, 0).Unix()
-	} else {
-		f, _ := strconv.Atoi(fromParam)
-		from = int64(f)
+	offset := 0
+	size := 10
+	sortDB := []string{}
+	if sortType != "asc" && sortType != "dec" {
+		sortType = "asc"
+	}
+	if sortBy != "" {
+		if val, ok := sortedList[sortBy]; ok {
+			if sortType == "asc" {
+				sortDB = append(sortDB, "+"+val)
+			} else {
+				sortDB = append(sortDB, "-"+val)
+			}
+
+		}
+	}
+	if pageOffset != "" {
+		t, err := strconv.Atoi(pageOffset)
+		if err != nil {
+			httputils.WriteError(w, http.StatusBadRequest, "Invalid page offset")
+			return
+		}
+		offset = t
+	}
+	if pageSize != "" {
+		t, err := strconv.Atoi(pageSize)
+		if err != nil {
+			httputils.WriteError(w, http.StatusBadRequest, "Invalid page size")
+			return
+		}
+		size = t
 	}
 
-	limit := types.DefaultLimit
-	if l != "" {
-		limit, _ = strconv.Atoi(l)
-	}
-
-	baseToken := common.HexToAddress(bt)
-	quoteToken := common.HexToAddress(qt)
-	res, err := e.tradeService.GetSortedTrades(baseToken, quoteToken, from, to, limit)
+	res, err := e.tradeService.GetTrades(&tradeSpec, sortDB, offset, size)
 	if err != nil {
 		logger.Error(err)
 		httputils.WriteError(w, http.StatusInternalServerError, "")
