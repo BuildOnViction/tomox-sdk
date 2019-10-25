949ce4fe29eab5a868ebcc40dfa77a882e694fb7
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2019-10-25 06:13:29 +0000

    update watch option

--- daos/trade.go
+++ daos/trade.go
@@ -77,7 +77,10 @@ func (dao *TradeDao) GetCollection() *mgo.Collection {
 }
 
 func (dao *TradeDao) Watch() (*mgo.ChangeStream, *mgo.Session, error) {
-	return db.Watch(dao.dbName, dao.collectionName, mgo.ChangeStreamOptions{FullDocument: mgo.UpdateLookup})
+	return db.Watch(dao.dbName, dao.collectionName, mgo.ChangeStreamOptions{
+        MaxAwaitTimeMS: 500,
+        BatchSize: 1000,
+    })
 }
 
 // Create function performs the DB insertion task for trade collection
