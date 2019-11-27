ac91a7ee6ce0551ebfafab9ab92a0f1681e0f776
tran nghia <nghiatran@tomochain.com>
2019-11-27 14:17:31 +0700

    remove redundant file

--- price_broard.py
+++
@@ -1,6 +0,0 @@
-from websocket import create_connection
-ws = create_connection("ws://127.0.0.1:8080/socket")
-ws.send('{"channel": "price_board","event": {"type": "UNSUBSCRIBE","payload": null}}')
-result =  ws.recv()
-print("Received '%s'" % result)
-ws.close()
--- test.py
+++
@@ -1,7 +0,0 @@
-from websocket import create_connection
-#import socket
-ws = create_connection("ws://127.0.0.1:8080/socket")
-ws.send('{"channel": "orders","event": {"type": "NEW_ORDER","hash": null,"payload": {}}}')
-result =  ws.recv()
-print("Received '%s'" % result)
-ws.close()
