954c8524cb4feb878072eb9c40f3fd146aa58546
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2020-04-02 02:54:39 +0000

    update new pair realtime

--- crons/lending_priceboard.go
+++ crons/lending_priceboard.go
@@ -3,7 +3,6 @@ package crons
 import (
 	"log"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/robfig/cron"
 	"github.com/tomochain/tomox-sdk/utils"
 	"github.com/tomochain/tomox-sdk/ws"
@@ -12,23 +11,25 @@ import (
 // tickStreamingCron takes instance of cron.Cron and adds tickStreaming
 // crons according to the durations mentioned in config/app.yaml file
 func (s *CronService) startLendingPriceBoardCron(c *cron.Cron) {
-	pairs, err := s.lendingPairService.GetAll()
-
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	for _, p := range pairs {
-		c.AddFunc("*/3 * * * * *", s.getLendingPriceBoardData(p.Term, p.LendingTokenAddress))
-	}
+	c.AddFunc("*/3 * * * * *", s.getLendingPriceBoardData())
 }
 
 // tickStream function fetches latest tick based on unit and duration for each pair
 // and broadcasts the tick to the client subscribed to pair's respective channel
-func (s *CronService) getLendingPriceBoardData(term uint64, lendingToken common.Address) func() {
+func (s *CronService) getLendingPriceBoardData() func() {
 	return func() {
-		id := utils.GetLendingChannelID(term, lendingToken)
-		tick := s.lendingPriceBoardService.GetLendingPriceBoardData(term, lendingToken)
-		ws.GetLendingPriceBoardSocket().BroadcastMessage(id, tick)
+		pairs, err := s.lendingPairService.GetAll()
+
+		if err != nil {
+			log.Println(err.Error())
+		}
+
+		for _, p := range pairs {
+			term := p.Term
+			lendingToken := p.LendingTokenAddress
+			id := utils.GetLendingChannelID(term, lendingToken)
+			tick := s.lendingPriceBoardService.GetLendingPriceBoardData(term, lendingToken)
+			ws.GetLendingPriceBoardSocket().BroadcastMessage(id, tick)
+		}
 	}
 }
--- crons/priceBoard.go
+++ crons/priceBoard.go
@@ -5,7 +5,6 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/robfig/cron"
 	"github.com/tomochain/tomox-sdk/types"
 	"github.com/tomochain/tomox-sdk/utils"
@@ -15,69 +14,71 @@ import (
 // tickStreamingCron takes instance of cron.Cron and adds tickStreaming
 // crons according to the durations mentioned in config/app.yaml file
 func (s *CronService) startPriceBoardCron(c *cron.Cron) {
-	pairs, err := s.PairService.GetAll()
-
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	for _, p := range pairs {
-		c.AddFunc("*/3 * * * * *", s.getPriceBoardData(p.BaseTokenAddress, p.QuoteTokenAddress))
-	}
+	c.AddFunc("*/3 * * * * *", s.getPriceBoardData())
 }
 
 // tickStream function fetches latest tick based on unit and duration for each pair
 // and broadcasts the tick to the client subscribed to pair's respective channel
-func (s *CronService) getPriceBoardData(bt, qt common.Address) func() {
+func (s *CronService) getPriceBoardData() func() {
 	return func() {
-		p := make([]types.PairAddresses, 0)
-		p = []types.PairAddresses{{
-			BaseToken:  bt,
-			QuoteToken: qt,
-		}}
-
-		// Fix the value at 1 day because we only care about 24h change
-		duration := int64(1)
-		unit := "day"
-
-		ticks, err := s.PriceBoardService.GetPriceBoardData(p, duration, unit)
-		if err != nil {
-			log.Printf("%s", err)
-			return
-		}
-
-		quoteToken, err := s.PriceBoardService.TokenDao.GetByAddress(qt)
+		pairs, err := s.PairService.GetAll()
 
 		if err != nil {
-			log.Printf("%s", err)
-			return
+			log.Println(err.Error())
 		}
 
-		var lastTradePrice string
-		lastTrade, err := s.PriceBoardService.TradeDao.GetLatestTrade(bt, qt)
-		if lastTrade == nil {
-			lastTradePrice = "?"
-		} else {
-			lastTradePrice = lastTrade.PricePoint.String()
-		}
+		for _, p := range pairs {
+			bt := p.BaseTokenAddress
+			qt := p.QuoteTokenAddress
+			p := make([]types.PairAddresses, 0)
+			p = []types.PairAddresses{{
+				BaseToken:  bt,
+				QuoteToken: qt,
+			}}
 
-		if err != nil {
-			log.Printf("%s", err)
-			return
-		}
+			// Fix the value at 1 day because we only care about 24h change
+			duration := int64(1)
+			unit := "day"
 
-		id := utils.GetPriceBoardChannelID(bt, qt)
-		var usd *big.Float
-		usd, e := s.OHLCVService.GetLastPriceCurrentByTime(quoteToken.Symbol, time.Now())
-		if e != nil || usd == nil {
-			usd = big.NewFloat(0)
-		}
-		result := types.PriceBoardData{
-			Ticks:          ticks,
-			PriceUSD:       usd.String(),
-			LastTradePrice: lastTradePrice,
-		}
+			ticks, err := s.PriceBoardService.GetPriceBoardData(p, duration, unit)
+			if err != nil {
+				log.Printf("%s", err)
+				return
+			}
 
-		ws.GetPriceBoardSocket().BroadcastMessage(id, result)
+			quoteToken, err := s.PriceBoardService.TokenDao.GetByAddress(qt)
+
+			if err != nil {
+				log.Printf("%s", err)
+				return
+			}
+
+			var lastTradePrice string
+			lastTrade, err := s.PriceBoardService.TradeDao.GetLatestTrade(bt, qt)
+			if lastTrade == nil {
+				lastTradePrice = "?"
+			} else {
+				lastTradePrice = lastTrade.PricePoint.String()
+			}
+
+			if err != nil {
+				log.Printf("%s", err)
+				return
+			}
+
+			id := utils.GetPriceBoardChannelID(bt, qt)
+			var usd *big.Float
+			usd, e := s.OHLCVService.GetLastPriceCurrentByTime(quoteToken.Symbol, time.Now())
+			if e != nil || usd == nil {
+				usd = big.NewFloat(0)
+			}
+			result := types.PriceBoardData{
+				Ticks:          ticks,
+				PriceUSD:       usd.String(),
+				LastTradePrice: lastTradePrice,
+			}
+
+			ws.GetPriceBoardSocket().BroadcastMessage(id, result)
+		}
 	}
 }
